cec3d6cb9aad508bc0f7ef83c0724da11a13c054
jermon <jerker.montelius@gmail.com>
2023-08-10 16:24:42 +0200

    General update

--- _bin/generate_indexfile.sh
+++ _bin/generate_indexfile.sh
@@ -3,6 +3,9 @@
 #define parameters which are passed in.
 basename=$1
 title=`echo $basename | tr _ " "`
+parts=`unzip -c ${basename}.mscz ${basename}.mscx | grep '<longName>' | cut -d'>' -f2 | cut -d'<' -f1 | sort -u`
+categories=`unzip -c ${basename}.mscz ${basename}.mscx |grep '<metaTag name="categories">'|cut -d'>' -f2|cut -d'<' -f1|sort -u`
+tags=`unzip -c ${basename}.mscz ${basename}.mscx |grep '<metaTag name="tags">'|cut -d'>' -f2|cut -d'<' -f1|sort -u`
 
 #define the template.
 cat  << EOF
@@ -10,15 +13,25 @@ cat  << EOF
 layout: default
 title: ${title}
 basename: ${basename}
-parts:
-  - Soprano
-  - Alto
-  - Tenor
-  - Bass
+categories: ${categories}
+tags:
+EOF
 
+for tag in $tags 
+do
+  echo "  - " $tag
+done
+
+echo "parts:"
+
+for part in $parts 
+do
+  echo "  - " $part
+done
+
+cat  << EOF
 ---
 
 {% include mscweb.html %}
 
 EOF
-
